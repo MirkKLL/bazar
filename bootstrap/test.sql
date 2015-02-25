49dd3443a6c833d28cbe8aecdbc277ef6adbf646
MirkiLL <to.megbeg@gmail.com>
2015-02-25 15:55:14 +0200

    sql update

--- bootstrap/test.sql
+++ bootstrap/test.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.3.8
 -- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1
--- Время создания: Фев 19 2015 г., 14:01
--- Версия сервера: 5.6.17
--- Версия PHP: 5.5.12
+-- Хост: localhost
+-- Время создания: Фев 25 2015 г., 15:54
+-- Версия сервера: 5.5.41-0ubuntu0.14.04.1
+-- Версия PHP: 5.5.9-1ubuntu4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -22,26 +22,28 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `food`;
 CREATE TABLE IF NOT EXISTS `food` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `id` int(5) NOT NULL,
   `name` char(128) NOT NULL,
   `description` varchar(512) NOT NULL,
   `photo_id` int(5) NOT NULL,
   `amount` float NOT NULL,
   `measure` enum('л.','кг.','шт.') NOT NULL,
   `category` int(2) NOT NULL,
+  `owner_id` int(8) NOT NULL,
   `last_price` float NOT NULL,
   `prod_date` date NOT NULL,
   `expire_date` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `food`
 --
 
-INSERT INTO `food` (`id`, `name`, `description`, `photo_id`, `amount`, `measure`, `category`, `last_price`, `prod_date`, `expire_date`) VALUES
-(1, 'Молоко домашнее', 'С села', 1, 1, 'л.', 15, 9.53, '2015-01-26', '2015-01-31'),
-(2, 'Синеглазка', 'Картошка синяя', 2, 1, 'кг.', 15, 4.4, '2015-01-01', '2015-03-31');
+INSERT INTO `food` (`id`, `name`, `description`, `photo_id`, `amount`, `measure`, `category`, `owner_id`, `last_price`, `prod_date`, `expire_date`, `updated_at`) VALUES
+(1, 'Молоко домашнее', 'С села', 1, 1, 'л.', 15, 4, 19.54, '2015-01-26', '2015-01-31', '2015-02-25 13:51:48'),
+(2, 'Синеглазка', 'Картошка синяя', 2, 1, 'кг.', 15, 5, 4, '2015-01-01', '2015-03-31', '2015-02-25 13:51:48'),
+(3, 'Рис шлифованый', 'на станке шлифовочном\n(500 гр)', 0, 1, 'шт.', 18, 4, 17, '0000-00-00', '0000-00-00', '2015-02-25 13:52:26');
 
 -- --------------------------------------------------------
 
@@ -51,12 +53,11 @@ INSERT INTO `food` (`id`, `name`, `description`, `photo_id`, `amount`, `measure`
 
 DROP TABLE IF EXISTS `food__category`;
 CREATE TABLE IF NOT EXISTS `food__category` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `id` int(3) NOT NULL,
   `parent_id` int(5) unsigned NOT NULL DEFAULT '0',
   `name` char(254) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+  `description` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `food__category`
@@ -91,14 +92,13 @@ INSERT INTO `food__category` (`id`, `parent_id`, `name`, `description`) VALUES
 
 DROP TABLE IF EXISTS `locations`;
 CREATE TABLE IF NOT EXISTS `locations` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `id` int(5) NOT NULL,
   `address` char(255) NOT NULL,
   `user_id` int(5) NOT NULL,
   `apt` int(11) NOT NULL,
   `lat` int(11) NOT NULL,
-  `photo` int(5) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
+  `photo` int(5) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `locations`
@@ -133,7 +133,9 @@ INSERT INTO `locations` (`id`, `address`, `user_id`, `apt`, `lat`, `photo`) VALU
 (26, 'fffff', 1, 0, 0, 0),
 (27, 'fffff', 1, 0, 0, 0),
 (28, 'fffff', 1, 0, 0, 0),
-(29, 'fffff', 1, 0, 0, 0);
+(29, 'fffff', 1, 0, 0, 0),
+(30, 'пр.Ленина 52', 1, 0, 0, 0),
+(31, 'my addres', 1, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -143,7 +145,7 @@ INSERT INTO `locations` (`id`, `address`, `user_id`, `apt`, `lat`, `photo`) VALU
 
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE IF NOT EXISTS `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user` int(11) NOT NULL,
   `status` int(11) NOT NULL,
   `location` int(11) NOT NULL,
@@ -151,35 +153,16 @@ CREATE TABLE IF NOT EXISTS `order` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `coments` varchar(254) NOT NULL,
   `price` float NOT NULL,
-  `ip` varchar(15) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+  `ip` varchar(15) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `order`
 --
 
 INSERT INTO `order` (`id`, `user`, `status`, `location`, `created_at`, `updated_at`, `coments`, `price`, `ip`) VALUES
-(1, 1, 1, 1, '2015-02-05 00:00:00', '2015-02-05 19:13:58', '', 553, ''),
-(2, 3, 1, 7, '0000-00-00 00:00:00', '2015-02-05 21:21:32', 'coment', 35.2, '127.0.0.1'),
-(3, 3, 1, 8, '0000-00-00 00:00:00', '2015-02-05 21:22:34', 'coment', 35.2, '127.0.0.1'),
-(4, 3, 1, 9, '0000-00-00 00:00:00', '2015-02-05 21:24:39', 'coment', 35.2, '127.0.0.1'),
-(5, 3, 1, 12, '0000-00-00 00:00:00', '2015-02-05 21:27:03', 'coment', 35.2, '127.0.0.1'),
-(6, 3, 1, 13, '0000-00-00 00:00:00', '2015-02-05 21:36:06', 'coment', 35.2, '127.0.0.1'),
-(7, 3, 1, 16, '2015-02-05 23:37:11', '2015-02-05 21:37:11', 'coment', 35.2, '127.0.0.1'),
-(8, 3, 1, 17, '2015-02-05 23:38:28', '2015-02-05 21:38:28', 'coment', 35.2, '127.0.0.1'),
-(9, 3, 1, 18, '2015-02-05 23:49:07', '2015-02-05 21:49:07', 'coment', 35.2, '127.0.0.1'),
-(10, 3, 1, 19, '2015-02-05 23:49:17', '2015-02-05 21:49:17', 'coment', 35.2, '127.0.0.1'),
-(11, 1, 1, 20, '2015-02-05 23:50:22', '2015-02-05 21:50:22', 'test', 63.79, '127.0.0.1'),
-(12, 1, 1, 21, '2015-02-05 23:54:07', '2015-02-05 21:54:07', 'test', 63.79, '127.0.0.1'),
-(13, 1, 2, 22, '2015-02-05 23:54:11', '2015-02-19 12:59:55', 'Make it easy', 63.79, '127.0.0.1'),
-(14, 1, 1, 23, '2015-02-05 23:55:17', '2015-02-05 21:55:17', 'test', 63.79, '127.0.0.1'),
-(15, 1, 1, 24, '2015-02-05 23:55:56', '2015-02-05 21:55:56', 'no', 63.79, '127.0.0.1'),
-(16, 1, 1, 25, '2015-02-05 23:56:54', '2015-02-05 21:56:54', 'no', 63.79, '127.0.0.1'),
-(17, 1, 1, 26, '2015-02-05 23:56:57', '2015-02-05 21:56:57', 'no', 63.79, '127.0.0.1'),
-(18, 1, 1, 27, '2015-02-05 23:57:10', '2015-02-05 21:57:10', 'no', 63.79, '127.0.0.1'),
-(19, 1, 1, 28, '2015-02-05 23:57:31', '2015-02-05 21:57:31', 'no', 63.79, '127.0.0.1'),
-(20, 1, 1, 29, '2015-02-05 23:57:52', '2015-02-05 21:57:52', 'no', 63.79, '127.0.0.1');
+(1, 1, 2, 30, '2015-02-19 18:32:53', '2015-02-21 22:04:51', 'Доставьте побыстрее', 28.59, '127.0.0.1'),
+(2, 1, 1, 31, '2015-02-22 00:04:25', '2015-02-21 22:05:15', 'my coments for order', 56.45, '127.0.0.1');
 
 -- --------------------------------------------------------
 
@@ -192,9 +175,8 @@ CREATE TABLE IF NOT EXISTS `order__detail` (
   `order_id` int(11) NOT NULL,
   `food_id` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `subtotal` int(11) NOT NULL,
-  KEY `order_id` (`order_id`,`food_id`)
+  `price` float NOT NULL,
+  `subtotal` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -202,28 +184,9 @@ CREATE TABLE IF NOT EXISTS `order__detail` (
 --
 
 INSERT INTO `order__detail` (`order_id`, `food_id`, `qty`, `price`, `subtotal`) VALUES
-(1, 1, 3, 33, 0),
-(10, 2, 8, 4, 35),
-(11, 2, 8, 4, 35),
-(11, 1, 3, 10, 29),
-(12, 2, 8, 4, 35),
-(12, 1, 3, 10, 29),
-(13, 2, 8, 4, 35),
-(13, 1, 3, 10, 29),
-(14, 2, 8, 4, 35),
-(14, 1, 3, 10, 29),
-(15, 2, 8, 4, 35),
-(15, 1, 3, 10, 29),
-(16, 2, 8, 4, 35),
-(16, 1, 3, 10, 29),
-(17, 2, 8, 4, 35),
-(17, 1, 3, 10, 29),
-(18, 2, 8, 4, 35),
-(18, 1, 3, 10, 29),
-(19, 2, 8, 4, 35),
-(19, 1, 3, 10, 29),
-(20, 2, 8, 4, 35),
-(20, 1, 3, 10, 29);
+(1, 1, 3, 9.53, 28.59),
+(2, 1, 5, 9.53, 47.65),
+(2, 2, 2, 4.4, 8.8);
 
 -- --------------------------------------------------------
 
@@ -233,11 +196,10 @@ INSERT INTO `order__detail` (`order_id`, `food_id`, `qty`, `price`, `subtotal`) 
 
 DROP TABLE IF EXISTS `order__status`;
 CREATE TABLE IF NOT EXISTS `order__status` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(125) NOT NULL,
-  `short` varchar(54) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+  `short` varchar(54) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `order__status`
@@ -260,11 +222,10 @@ INSERT INTO `order__status` (`id`, `name`, `short`) VALUES
 
 DROP TABLE IF EXISTS `photo`;
 CREATE TABLE IF NOT EXISTS `photo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `path` varchar(256) NOT NULL,
-  `alt` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `alt` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -276,8 +237,7 @@ DROP TABLE IF EXISTS `price`;
 CREATE TABLE IF NOT EXISTS `price` (
   `food_id` int(5) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `price` float NOT NULL,
-  KEY `food_id` (`food_id`,`updated_at`)
+  `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -301,7 +261,7 @@ CREATE TABLE IF NOT EXISTS `price_history` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `id` int(4) NOT NULL,
   `phone` char(10) NOT NULL,
   `first_name` char(15) DEFAULT NULL,
   `last_name` char(15) DEFAULT NULL,
@@ -311,19 +271,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `blocked` tinyint(1) NOT NULL DEFAULT '0',
   `bonus` decimal(2,2) NOT NULL DEFAULT '0.00',
   `notes` char(255) DEFAULT NULL,
-  `password` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `phone` (`phone`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `password` varchar(256) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `phone`, `first_name`, `last_name`, `email`, `photo`, `role_id`, `blocked`, `bonus`, `notes`, `password`) VALUES
-(1, '0938787172', 'Yevgeniy', 'Sidelnikov', 'to.MegBeg@gmail.com', NULL, 3, 0, '0.00', NULL, '202cb962ac59075b964b07152d234b70'),
-(2, '7777777776', 'ivan', 'testov', '', NULL, 0, 0, '0.00', NULL, ''),
-(3, '7777777777', 'ivan', 'testov', '', NULL, 0, 0, '0.00', NULL, '');
+(1, '0938787172', 'Yevgeniy', 'Sidelnikov', 'to.MegBeg@gmail.com', NULL, 5, 0, 0.00, NULL, '202cb962ac59075b964b07152d234b70'),
+(2, '7777777776', 'ivan', 'unregistred', '', NULL, 0, 0, 0.00, NULL, ''),
+(3, '7777777777', 'ivan', 'registred', '', NULL, 1, 0, 0.00, NULL, ''),
+(4, '2222222222', 'owner', 'seller', '', NULL, 2, 0, 0.00, NULL, ''),
+(5, '333333333', 'phone', 'operator', '', NULL, 3, 0, 0.00, NULL, ''),
+(6, '444444444', 'packed', 'man', '', NULL, 4, 0, 0.00, NULL, '');
 
 -- --------------------------------------------------------
 
@@ -333,10 +294,9 @@ INSERT INTO `users` (`id`, `phone`, `first_name`, `last_name`, `email`, `photo`,
 
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE IF NOT EXISTS `user_role` (
-  `id` int(2) NOT NULL AUTO_INCREMENT,
-  `name` char(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `id` int(2) NOT NULL,
+  `name` char(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `user_role`
@@ -346,3 +306,112 @@ INSERT INTO `user_role` (`id`, `name`) VALUES
 (1, 'user'),
 (2, 'worker'),
 (3, 'admin');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `food__category`
+--
+ALTER TABLE `food__category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `order__detail`
+--
+ALTER TABLE `order__detail`
+  ADD UNIQUE KEY `order_id` (`order_id`,`food_id`);
+
+--
+-- Индексы таблицы `order__status`
+--
+ALTER TABLE `order__status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `photo`
+--
+ALTER TABLE `photo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `price`
+--
+ALTER TABLE `price`
+  ADD KEY `food_id` (`food_id`,`updated_at`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `phone` (`phone`);
+
+--
+-- Индексы таблицы `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `food`
+--
+ALTER TABLE `food`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `food__category`
+--
+ALTER TABLE `food__category`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT для таблицы `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
+--
+-- AUTO_INCREMENT для таблицы `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `order__status`
+--
+ALTER TABLE `order__status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT для таблицы `photo`
+--
+ALTER TABLE `photo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT для таблицы `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
